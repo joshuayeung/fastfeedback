58d7e599ddb7f4a1250d30765b0fef17f2e9ea95
Joshua Yeung <joshua.chyeung@gmail.com>
2020-12-28 21:13:07 +0800

    feat: sites api using firebase admin

---
+++ pages/api/sites.js
@@ -0,0 +1,20 @@
+import db from "@/lib/db";
+
+async function handler(_, res) {
+  const sitesRef = db.collection("sites");
+  const snapshot = await sitesRef.get();
+  if (snapshot.empty) {
+    console.log("No matching documents.");
+    return;
+  }
+
+  const sites = [];
+
+  snapshot.forEach((doc) => {
+    sites.push({ id: doc.id, ...doc.data() });
+  });
+  console.log(sites);
+  res.status(200).json(sites);
+}
+
+export default handler;
